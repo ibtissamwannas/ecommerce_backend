a8ea95891b0405b983cfb7bae7768ce74486cf15
ibtissamwannas <ibtissamwannas21@gmail.com.com>
2023-05-28 13:04:35 +0300

    login done

--- auth/login.php
+++ auth/login.php
@@ -5,8 +5,10 @@ include "../connect.php";
 $email = filterRequest("email");
 $password = sha1($_POST["password"]);
 
-$stmt = $con->prepare("SELECT * FROM users WHERE users_email = ? AND users_password = ? AND users_approve = 1");
-$stmt->execute(array($email,$password));
-$count = $stmt->rowCount();
-result($count,"none","email or password are not correct");
+// $stmt = $con->prepare("SELECT * FROM users WHERE users_email = ? AND users_password = ? AND users_approve = 1");
+// $stmt->execute(array($email,$password));
+// $count = $stmt->rowCount();
+// result($count,"none","email or password are not correct");
+// same func
+getData("users","users_email = ? AND users_password = ? AND users_approve = 1",array($email,$password));
 ?>
--- functions.php
+++ functions.php
@@ -23,6 +23,22 @@ function getAllData($table, $where = null, $values = null)
     return $count;
 }
 
+function getData($table, $where = null, $values = null)
+{
+    global $con;
+    $data = array();
+    $stmt = $con->prepare("SELECT  * FROM $table WHERE $where ");
+    $stmt->execute($values);
+    $data = $stmt->fetch(PDO::FETCH_ASSOC);
+    $count  = $stmt->rowCount();
+    if ($count > 0){
+        echo json_encode(array("status" => "success", "data" => $data));
+    } else {
+        echo json_encode(array("status" => "failure"));
+    }
+    return $count;
+}
+
 function insertData($table, $data, $json = true)
 {
     global $con;
